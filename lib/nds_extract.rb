5edfe423efbc8238101eee4891063d5167ca72df
Cole Davis <colebuildanddevelop@gmail.com>
2020-03-13 20:06:13 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -61,7 +61,7 @@ def movies_with_director_key(name, movies_collection)
   array
 end
 
-movies_with_director_key('Stephen SpielBerg', db[0][:movies])
+puts movies_with_director_key('Stephen SpielBerg', db[0][:movies])
 
 def gross_per_studio(collection)
   # GOAL: Given an Array of Hashes where each Hash represents a movie,
