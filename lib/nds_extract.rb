55d93074e2da6164644f4efda95f85a4ff3a8f07
Cole Davis <colebuildanddevelop@gmail.com>
2020-03-13 19:44:20 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -22,6 +22,38 @@ def flatten_a_o_a(aoa)
   result
 end
 
+def movie_with_director_name(director_name, movie_data)
+  { 
+    :title => movie_data[:title],
+    :worldwide_gross => movie_data[:worldwide_gross],
+    :release_year => movie_data[:release_year],
+    :studio => movie_data[:studio],
+    :director_name => director_name
+  }
+end
+
+
+# Your code after this point
+
+def movies_with_director_key(name, movies_collection)
+  # GOAL: For each Hash in an Array (movies_collection), provide a collection
+  # of movies and a directors name to the movie_with_director_name method
+  # and accumulate the returned Array of movies into a new Array that's
+  # returned by this method.
+  #
+  # INPUT:
+  # * name: A director's name
+  # * movies_collection: An Array of Hashes where each Hash represents a movie
+  #
+  # RETURN:
+  #
+  # Array of Hashes where each Hash represents a movie; however, they should all have a
+  # :director_name key. This addition can be done by using the provided
+  # movie_with_director_name method
+  
+end
+
+
 def gross_per_studio(collection)
   # GOAL: Given an Array of Hashes where each Hash represents a movie,
   # return a Hash that includes the total worldwide_gross of all the movies from
@@ -43,7 +75,8 @@ def gross_per_studio(collection)
     hash[:studio] += worldwide_gross
     index += 1
   end
-  hash
+  
+  
 end
 
 def movies_with_directors_set(source)
@@ -57,6 +90,7 @@ def movies_with_directors_set(source)
   #
   # Array of Arrays containing all of a director's movies. Each movie will need
   # to have a :director_name key added to it.
+  
   index = 0
   array = []
   while source[index] do 
@@ -65,6 +99,11 @@ def movies_with_directors_set(source)
   end 
   array
 end
+
+pp gross_per_studio(flatten_a_o_a(movies_with_directors_set(db)))
+
+
+
 
 
 # ----------------    End of Your Code Region --------------------
@@ -76,5 +115,3 @@ def studios_totals(nds)
   movies_with_director_names = flatten_a_o_a(a_o_a_movies_with_director_names)
   return gross_per_studio(movies_with_director_names)
 end
-
-puts studios_totals(db)
