54872ed69d38785b920362b03cc47807aef5f400
Cole Davis <colebuildanddevelop@gmail.com>
2020-03-13 19:37:52 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -51,16 +51,8 @@ def movies_with_director_key(name, movies_collection)
   # :director_name key. This addition can be done by using the provided
   # movie_with_director_name method
   
-  array_of_hashes = []
-  index = 0
-  while movies_collection[index] do
-    array_of_hashes = movie_with_director_name(name, movies_collection[index][:movies])
-    index += 1
-  end
-  array_of_hashes
 end
 
-puts movies_with_director_names("Stephen Spielberg", db)
 
 def gross_per_studio(collection)
   # GOAL: Given an Array of Hashes where each Hash represents a movie,
@@ -87,9 +79,17 @@ def movies_with_directors_set(source)
   #
   # Array of Arrays containing all of a director's movies. Each movie will need
   # to have a :director_name key added to it.
+  
+  index = 0
+  array = []
+  while source[index] do 
+    array << source[index][:movies]
+    index += 1 
+  end 
+  array
 end
 
-
+movies_with_directors_set(db)
 
 
 
